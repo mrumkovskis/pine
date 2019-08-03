0cda3c888d43ca653925d1ebfe6a520015d01be9
Martins Rumkovskis <martins@uniso.lv>
2019-08-04 00:46:22 +0300

    cmd chain refactoring

--- src/CmdChain.elm
+++ src/CmdChain.elm
@@ -1,5 +1,5 @@
 module CmdChain exposing
-  ( Model, Msg, Tomsg, init, exec, update, isDone
+  ( Msg, Tomsg, exec, update
   )
 
 
@@ -10,33 +10,15 @@ import Task
 import Debug exposing (log, toString)
 
 
-type Model msg model
-   = Model (ModelInternal msg model)
-
-
-type alias ModelInternal msg model =
-  { updater: msg -> model -> (model, Cmd msg)
-  , modelUpdater: model -> Model msg model -> model
-  , cmdChain: List (Cmd msg)
-  , done: Bool
-  }
+type alias Updater msg model = msg -> model -> (model, Cmd msg)
 
 
 type Msg msg
   = ExecMsg (List (Cmd msg))
-  | DoNextMsg msg
+  | DoNextMsg (List (Cmd msg)) msg
 
 
 type alias Tomsg msg = Msg msg -> msg
-
-
-init: (msg -> model -> (model, Cmd msg)) -> (model -> Model msg model -> model) -> Model msg model
-init updater modelUpdater =
-  Model <| ModelInternal updater modelUpdater [] False
-
-
-isDone: Model msg model -> Bool
-isDone (Model { done }) = done
 
 
 exec: Tomsg msg -> List (Cmd msg) -> Cmd msg
@@ -44,24 +26,23 @@ exec toMsg cmds =
   do (toMsg << ExecMsg) <| cmds
 
 
-update: Tomsg msg -> Msg msg -> Model msg model -> model -> (model, Cmd msg)
-update toMsg msg (Model ({ updater, modelUpdater, cmdChain } as cmodel)) model =
+update: Updater msg model -> Tomsg msg -> Msg msg -> model -> (model, Cmd msg)
+update updater toMsg msg model =
   case msg of
     ExecMsg cmds ->
-      ( List.tail cmds |>
-        Maybe.map (\rest -> { cmodel | cmdChain = rest}) |>
-        Maybe.withDefault cmodel
-      , List.head cmds |>
-        Maybe.map (Cmd.map (toMsg << DoNextMsg)) |>
-        Maybe.withDefault Cmd.none
-      ) |>
-      (\(cm, cmd) -> ( modelUpdater model <| Model { cm | done = cmd == Cmd.none }, cmd ))
+      case cmds of
+        [] ->
+          ( model, Cmd.none )
 
-    DoNextMsg nmsg ->
+        cmd :: rest ->
+          ( model, Cmd.map (toMsg << DoNextMsg rest) cmd)
+
+    DoNextMsg cmds nmsg ->
       updater nmsg model |>
       Tuple.mapSecond
         (\cmd ->
           if cmd == Cmd.none then
-            do (toMsg << ExecMsg) <| cmdChain
-          else Cmd.map (toMsg << DoNextMsg) cmd
+            do (toMsg << ExecMsg) cmds
+          else
+            Cmd.map (toMsg << DoNextMsg cmds) cmd
         )
