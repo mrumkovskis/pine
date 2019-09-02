973f651ad3007eac8dbdcae69f36a5ab68546f49
Martins Rumkovskis <martins@uniso.lv>
2019-09-02 09:59:35 +0300

    calendar

--- src/Calendar.elm
+++ src/Calendar.elm
@@ -5,9 +5,11 @@ module Calendar exposing
 
 import Json.Decode as JD
 import Json.Encode as JE
+import Dict
 
 import JsonModel as JM
 import Ask
+import Select exposing (..)
 
 
 type alias CalendarEntry =
@@ -63,6 +65,55 @@ decoder =
       ]
 
 
-calendar: Ask.Tomsg msg -> JM.ListModel msg CalendarEntry
+jsonDecoder: JD.Decoder (List JM.JsonValue)
+jsonDecoder =
+  decoder |>
+  JD.map
+    ( List.map
+        (\entry ->
+          [ ("value", JM.JsString entry.value)
+          , ("today", JM.JsString entry.today)
+          , ("month", JM.JsString entry.month)
+          , ("year", JM.JsString entry.year)
+          , ("date", JM.JsString entry.date)
+          , ("days", JM.JsList <| List.map JM.JsString entry.days)
+          , ("weeks", JM.JsList <| List.map JM.JsString entry.weeks)
+          , ("holidays"
+            , JM.JsList <| List.map
+                (\h ->
+                  [ ("date", JM.JsString h.date)
+                  , ("name", JM.JsString h.name)
+                  , ("description", JM.JsString h.description)
+                  , ("is_moved_working_day", JM.JsBool h.is_moved_working_day)
+                  ]
+                  |> Dict.fromList |>
+                  JM.JsObject
+                )
+                entry.holidays
+            )
+          ] |>
+          Dict.fromList |>
+          JM.JsObject
+        )
+    )
+
+
+calendar: Ask.Tomsg msg -> JM.ListModel msg JM.JsonValue
 calendar =
-  JM.initForm "/metadata" "/data" "/calendar" decoder (always JE.null) [] (always Nothing)
+  JM.initForm "/metadata" "/data" "/calendar" jsonDecoder (always JE.null) [] (always Nothing)
+
+
+calendarSelect: String -> Bool -> Tomsg msg JM.JsonValue -> Ask.Tomsg msg -> String -> (String -> msg)
+  -> (SelectModel msg JM.JsonValue, Cmd msg)
+calendarSelect locale doSearch toMsg toMessagemsg search toDestinationmsg =
+  ( Select.init
+      (calendar toMessagemsg)
+      locale
+      search
+      (JM.jsonString "value" >> Maybe.withDefault "<vērtība nav atrasta>" >> toDestinationmsg)
+      (JM.jsonString "value" >> Maybe.withDefault "<vērtība nav atrasta>")
+  , if doSearch then
+      Select.search toMsg search
+    else
+      Cmd.none
+  )
