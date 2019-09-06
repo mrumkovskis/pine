108f5572b2427ca6d30ff90c864139cc9661c4c3
Martins Rumkovskis <martins@uniso.lv>
2019-09-07 02:53:06 +0300

    date time formatting

--- src/Calendar.elm
+++ src/Calendar.elm
@@ -1,5 +1,6 @@
 module Calendar exposing
   ( CalendarEntry, Holiday
+  , formatDate, formatDateTime
   )
 
 
@@ -7,10 +8,15 @@ import Json.Decode as JD
 import Json.Encode as JE
 import Dict
 import Regex exposing (..)
+import Parser exposing (..)
+import Set
 
 import JsonModel as JM
 import Ask
 import Select exposing (..)
+
+
+import Debug
 
 
 type alias CalendarEntry =
@@ -120,52 +126,165 @@ calendarSelect locale doSearch toMsg toMessagemsg search toDestinationmsg =
   )
 
 
-format: String -> String -> String
-format pattern date =
-  ""
+formatDate: String -> String -> String
+formatDate mask value =
+  ymd value |>
+  (\(y, m, d) ->
+    format "yMd" mask (Dict.fromList [("y", y), ("M", m), ("d", d)])
+  ) |>
+  Maybe.withDefault value
+
+
+formatDateTime: String -> String -> String
+formatDateTime mask value =
+  ymd_hms value |>
+  (\((y, m, d), (h, min, s)) ->
+    format
+      "yMdhms"
+      mask
+      (Dict.fromList [("y", y), ("M", m), ("d", d), ("h", h), ("m", min), ("s", s)])
+  ) |>
+  Maybe.withDefault value
+
+
+format: String -> String -> Dict.Dict String String -> Maybe String
+format keys mask comps =
+  run (maskParser keys) mask |>
+  Result.toMaybe |>
+  Maybe.map
+    ( List.foldl
+        (\(_, key) res ->
+          let
+            getc c =
+              Dict.get c comps |>
+              Maybe.map adjust |>
+              Maybe.withDefault ""
+
+            adjust val =
+              let
+                  diff = String.length val - String.length key
+              in
+                if diff > 0 then
+                  String.dropLeft diff val
+                else
+                  String.padLeft diff '0' val
+          in
+            res ++
+            ( if String.contains "y" key then
+                getc "y"
+              else if String.contains "M" key then
+                getc "M"
+              else if String.contains "d" key then
+                getc "d"
+              else if String.contains "h" key then
+                getc "h"
+              else if String.contains "m" key then
+                getc "m"
+              else if String.contains "s" key then
+                getc "s"
+              else
+                key
+            )
+        )
+        ""
+    )
+
+
 
 parse: String -> String -> String
-parse pattern date =
+parse mask value =
   ""
 
 
 dateRegex: Regex
 dateRegex =
-  Regex.fromString "^(\\d{4})-(\\d{2})-(\\d{2})$" |> Maybe.withDefault Regex.never
+  r "^(\\d{4})-(\\d{1,2})-(\\d{1,2})$"
 
 
 dateTimeRegex: Regex
 dateTimeRegex =
-  Regex.fromString "^(\\d{4})-(\\d{2})-(\\d{2}).(\\d{2}):(\\d{2}):(\\d{2})$" |>
-  Maybe.withDefault Regex.never
+  r "^(\\d{4})-(\\d{1,2})-(\\d{1,2}).(\\d{1,2}):(\\d{1,2}):(\\d{1,2})$"
 
 
-separatorRegex: Regex
-separatorRegex =
-  Regex.fromString "[^\\w]" |> Maybe.withDefault Regex.never
+maskParser: String -> Parser (List ( Int, String ))
+maskParser keys =
+  let
+    keySet = String.split "" keys |> Set.fromList
+
+    sr = Regex.fromString "[^yMdhms]" |> Maybe.withDefault Regex.never
+
+    sep =
+      (getChompedString <| chompWhile (Regex.contains sr << String.fromChar)) |>
+      andThen (\res -> if String.isEmpty res then problem "not sep" else succeed res)
+
+    comp c =
+      (getChompedString <| chompWhile ((==) c)) |>
+      andThen (\res -> if String.isEmpty res then problem "not element" else succeed res)
+
+    compParser =
+      succeed Tuple.pair
+        |= getOffset
+        |= oneOf (sep :: (keys |> String.toList |> List.map comp))
+
+    validator =
+      andThen
+        (\comps ->
+          comps |>
+          List.map (\(_, s) -> String.left 1 s) |>
+          List.filter (\s -> Set.member s keySet) |>
+          (\res ->
+            if List.length res == Set.size (Set.fromList res) then
+              succeed comps
+            else
+              problem "invalid mask, pattern cannot repeat more than once"
+          )
+        )
+  in
+    (loop [] <|
+      (\comps ->
+        oneOf
+          [ succeed (\c -> Loop (c :: comps)) |= compParser
+          , succeed (Done <| List.reverse comps)
+          ]
+      )
+    ) |>
+    validator
 
 
-separators: String -> List (Int, String)
-separators pattern =
-  components separatorRegex pattern
+ymd: String -> (String, String, String)
+ymd date =
+  Regex.find dateRegex date |>
+  List.head |>
+  Maybe.map .submatches |>
+  Maybe.andThen
+    (\match ->
+      case match of
+        [ Just y, Just m, Just d ] ->
+          Just (y, m, d)
+
+        x ->
+          Nothing
+    ) |>
+  Maybe.withDefault ("<?>", "<?>", "<?>")
 
 
-dateMaskRegex: Regex
-dateMaskRegex =
-  Regex.fromString "(y{2,4}|M{2}|d{2}){1,3}" |> Maybe.withDefault Regex.never
+ymd_hms: String -> ((String, String, String), (String, String, String))
+ymd_hms dt =
+  Regex.find dateTimeRegex dt |>
+  List.head |>
+  Maybe.map .submatches |>
+  Maybe.andThen
+    (\match ->
+      case match of
+        [ Just y, Just m, Just d, Just h, Just min, Just s ] ->
+          Just ((y, m, d), (h, min, s))
+
+        x ->
+          Nothing
+    ) |>
+  Maybe.withDefault (("<?>", "<?>", "<?>"), ("<?>", "<?>", "<?>"))
 
 
-dateTimeMaskRegex: Regex
-dateTimeMaskRegex =
-  Regex.fromString "(y{2,4}|M{2}|d{2}|h{2}|m{2}|s{2}){1,6}" |> Maybe.withDefault Regex.never
-
-
-dateComponents: String -> List(Int, String)
-dateComponents pattern =
-  components dateMaskRegex pattern
-
-
-components: Regex -> String -> List (Int, String)
-components regex pattern =
-  Regex.find regex pattern |>
-  List.map (\m -> (m.index, m.match))
+r: String -> Regex
+r s =
+  Regex.fromString s |> Maybe.withDefault Regex.never
