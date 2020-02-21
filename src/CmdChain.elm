2045da18a101d024076abe13f7f7b0371dd5f189
Martins Rumkovskis <martins@uniso.lv>
2020-02-21 13:14:51 +0200

    Revert "InterruptMsg in cmd chain."
    This reverts commit 66538c3e1ebea1e97901445849677654c1232889.

--- src/CmdChain.elm
+++ src/CmdChain.elm
@@ -1,5 +1,5 @@
 module CmdChain exposing
-  ( Msg (..), Tomsg, exec, update
+  ( Msg, Tomsg, exec, update
   )
 
 
@@ -10,7 +10,6 @@ type alias Updater msg model = msg -> model -> (model, Cmd msg)
 
 type Msg msg
   = ExecMsg (List (Cmd msg)) (Maybe msg)
-  | InterruptMsg
 
 
 type alias Tomsg msg = Msg msg -> msg
@@ -47,6 +46,3 @@ update updater toMsg msg model =
             cmd :: rest ->
               Cmd.map (toMsg << ExecMsg rest << Just) cmd
         )
-
-    InterruptMsg ->
-      ( model, Cmd.none )
