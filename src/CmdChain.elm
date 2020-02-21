d24f8e95fa44973ce6d45f4e11bc8bc3154b469a
Martins Rumkovskis <martins@uniso.lv>
2020-02-21 19:39:02 +0200

    cmd chain interrupt support

--- src/CmdChain.elm
+++ src/CmdChain.elm
@@ -1,5 +1,5 @@
 module CmdChain exposing
-  ( Msg, Tomsg, exec, update
+  ( Msg, Tomsg, exec, interrupt, update
   )
 
 
@@ -10,9 +10,13 @@ type alias Updater msg model = msg -> model -> (model, Cmd msg)
 
 type Msg msg
   = ExecMsg (List (Cmd msg)) (Maybe msg)
+  | InterruptMsg
 
 
 type alias Tomsg msg = Msg msg -> msg
+
+
+type alias Frommsg msg = msg -> Maybe (Msg msg)
 
 
 exec: Tomsg msg -> List (Cmd msg) -> Cmd msg
@@ -20,22 +24,38 @@ exec toMsg cmds =
   do (toMsg << ExecMsg cmds) Nothing
 
 
-update: Updater msg model -> Tomsg msg -> Msg msg -> model -> (model, Cmd msg)
-update updater toMsg msg model =
+interrupt: Tomsg msg -> Cmd msg
+interrupt toMsg =
+  do toMsg InterruptMsg
+
+
+update: Updater msg model -> Frommsg msg -> Tomsg msg -> Msg msg -> model -> (model, Cmd msg)
+update updater fromMsg toMsg msg model =
   case msg of
     ExecMsg cmds mmsg ->
       mmsg |>
       Maybe.map
         (\modmsg ->
-            updater modmsg model |>
-            Tuple.mapSecond
-              (\cmd ->
-                if cmd == Cmd.none then
-                  do (toMsg << ExecMsg cmds) Nothing
-                else
-                  Cmd.map (toMsg << ExecMsg cmds << Just) cmd
-              )
+            fromMsg modmsg |>
+            Maybe.map
+              (\chmsg ->
+                case chmsg of
+                  ExecMsg chmds chmmsg ->
+                    ( model, do toMsg <| ExecMsg (chmds ++ cmds) chmmsg )
 
+                  InterruptMsg ->
+                    ( model, do toMsg InterruptMsg )
+              ) |>
+            Maybe.withDefault
+              ( updater modmsg model |>
+                Tuple.mapSecond
+                  (\cmd ->
+                    if cmd == Cmd.none then
+                      do (toMsg << ExecMsg cmds) Nothing
+                    else
+                      Cmd.map (toMsg << ExecMsg cmds << Just) cmd
+                  )
+              )
         ) |>
       Maybe.withDefault
         ( model
@@ -46,3 +66,6 @@ update updater toMsg msg model =
             cmd :: rest ->
               Cmd.map (toMsg << ExecMsg rest << Just) cmd
         )
+
+    InterruptMsg ->
+      ( model, Cmd.none )
